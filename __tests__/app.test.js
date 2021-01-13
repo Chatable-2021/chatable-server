fe2288297c731a1c3fd0e347e824bcfb8db2d34c
JustinMartin7x <justin.martin7x@gmail.com>
2021-01-13 14:31:55 -0800

    adjusted yml file temporarily to not run jest on merge

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,9 +2,20 @@ const fs = require('fs');
 const pool = require('../lib/utils/pool');
 const request = require('supertest');
 const app = require('../lib/app');
+const { hasUncaughtExceptionCaptureCallback } = require('process');
 
 describe('chatable-server routes', () => {
   beforeEach(() => {
     return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'))
   });
+
+  it('this is a template for future tests', () => {
+
+    const number = '10';
+
+
+    expect(number).toEqual('10')
+  } )
+
+
 });
