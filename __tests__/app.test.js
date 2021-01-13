090c31de34b003b6ab3a30e19e3ea797df95cd0f
JustinMartin7x <justin.martin7x@gmail.com>
2021-01-13 14:52:33 -0800

    delted beforeEach in test file

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,9 +4,8 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('chatable-server routes', () => {
-  // beforeEach(() => {
-  //   return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'))
-  // });
+  
+  
 
   it('this is a template for future tests', () => {
 
