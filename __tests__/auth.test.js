44d0838f337da78635fb25d8abb757117bd75bb1
JustinMartin7x <justin.martin7x@gmail.com>
2021-01-14 15:22:21 -0800

    add test to verify route

--- __tests__/auth.test.js
+++ __tests__/auth.test.js
@@ -17,12 +17,12 @@ describe('chatable-server routes', () => {
   it('should signup a user via POST', async() => {
     return await request(app)
       .post('/api/v1/auth/signup')
-      .send({ name: 'david', email: 'test@test.com', password: 'test' })
+      .send({ name: 'david', email: 'test@test1.com', password: 'test' })
       .then(res => {
         expect(res.body).toEqual({
           id: expect.any(String),
           name: 'david',
-          email: 'test@test.com',
+          email: 'test@test1.com',
         });
       });
   });
@@ -31,34 +31,38 @@ describe('chatable-server routes', () => {
     const response = await request(app)
       .post('/api/v1/auth/login')
       .send({ 
-        email: 'test@test.com',
+        email: 'test@test1.com',
         password: 'test' });
      
     expect(response.body).toEqual({
       id: expect.any(String),
       name: 'david',
-      email: 'test@test.com' });
+      email: 'test@test1.com' });
   });
 
-  it('should verify that a user is logged in', async() => {
+  it('verifies a user is logged in', async() => {
     const agent = request.agent(app);
     const user = await UserService.create({
       name: 'david',
-      email: 'test@test.com',
+      email: 'test@test2.com',
       password: 'test',
     });
+
     await agent
       .post('/api/v1/auth/login')
       .send({
-        email: 'test@test.com',
-        password: 'test',
-
+        email: 'test@test2.com',
+        password: 'test'
+      
       });
+
     const res = await agent
       .get('/api/v1/auth/verify');
+    
     expect(res.body).toEqual({
       id: user.id,
-      email: 'test@test.com',
+      name: 'david',
+      email: 'test@test2.com'
     });
   });
 
