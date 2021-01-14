d23380b71abdc9c0af7a955b1cce0ec9a0a07d1a
davidabutler92 <davidabutler92@yahoo.com>
2021-01-14 09:58:27 -0800

    add test for signup user

---
+++ __tests__/auth.test.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const pool = require('../lib/utils/pool');
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('chatable-server routes', () => {
+
+  beforeEach(() => {
+    return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'));
+  });
+
+  afterAll(() => {
+    return pool.end();
+  });
+
+  it('should signup a user via POST', async() => {
+    return await request(app)
+      .post('/api/v1/auth/signup')
+      .send({ name: 'david', email: 'test@test.com', password: 'test' })
+      .then(res => {
+        expect(res.body).toEqual({
+          id: expect.any(String),
+          name: 'david',
+          email: 'test@test.com',
+          password: 'test'
+        });
+      });
+  });
+});
