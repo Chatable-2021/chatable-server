fcef028f637b9f91300f5681f14a3fd4acdee331
davidabutler92 <davidabutler92@yahoo.com>
2021-01-15 13:18:26 -0800

    add socket server

---
+++ server.js
@@ -0,0 +1,24 @@
+const app = require('./lib/app');
+const pool = require('./lib/utils/pool');
+const socketio = require('socket.io');
+const http = require('http');
+
+const PORT = process.env.PORT || 7890;
+
+const server = http.createServer(app);
+const io = socketio(server);
+
+server.listen(PORT, () => {
+  console.log(`Started on ${PORT}`);
+})
+
+
+// app.listen(PORT, () => {
+//   // eslint-disable-next-line no-console
+//   console.log(`Started on ${PORT}`);
+// });
+
+process.on('exit', () => {
+  console.log('Goodbye!');
+  pool.end();
+});
