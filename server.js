48d26349c5df07a61eb0a2767d33469f53f39168
davidabutler92 <davidabutler92@yahoo.com>
2021-01-15 13:30:15 -0800

    add server is running on 7890

--- server.js
+++ server.js
@@ -12,7 +12,6 @@ server.listen(PORT, () => {
   console.log(`Started on ${PORT}`);
 })
 
-
 // app.listen(PORT, () => {
 //   // eslint-disable-next-line no-console
 //   console.log(`Started on ${PORT}`);
